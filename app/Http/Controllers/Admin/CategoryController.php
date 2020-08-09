b9c3ddbab3e45fe8875ae02388c2b5e672ab6305
Anton Aglushevich <aglanton31@ya.ru>
2020-08-09 17:07:16 +0300

    Request to CategoryRequest

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -38,7 +38,7 @@ class CategoryController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CategoryRequest $request)
     {
         $params = $request->all();
         unset($params['img']);
